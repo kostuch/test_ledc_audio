de6e41155b51db31f6e03fdd873650278ceccdcf
kostuch <kostuch@skeletondevices.com>
2022-02-06 19:32:53 +0100

    It works

--- src/main.cpp
+++ src/main.cpp
@@ -3,52 +3,42 @@
 
 #include "math.h"
 #include "stdio.h"
-#define PI_2 (6.283185307179f)
+#include "chirp22.h"
+#define PI_2 6.283185307179
 #define CONFIG_LEFT_CHANNEL_GPIO 19
 
-uint8_t *buf;
-uint32_t src_buf_size = 240;
-uint32_t buf_index;
-size_t cnt;
-uint16_t block_w = 256;
+#define AUDIO_SRC_SIZE 256
+#define RINGBUFFER_SIZE 1024
+int8_t buf[AUDIO_SRC_SIZE];
+bool replay = true;
+bool done;
 
 void sin_test()
 {
-	if (buf_index < src_buf_size)
-	{
-		if ((src_buf_size - buf_index) < block_w) block_w = src_buf_size - buf_index;
-
-		pwm_audio_write(buf + buf_index, block_w, &cnt, 0);
-		buf_index += cnt;
-	}
-	else
-	{
-		buf_index = 0;
-		block_w = 256;
-	}
+	static uint16_t buf_index;
+	size_t cnt;
+	pwm_audio_write((uint8_t*)(buf + buf_index), AUDIO_SRC_SIZE, &cnt, portMAX_DELAY);
+	buf_index = (buf_index + cnt) % AUDIO_SRC_SIZE;
 }
 
-void sin_test2()
+void wave_test()
 {
-	pwm_audio_write(buf + buf_index, block_w, &cnt, 0);
-	buf_index = (buf_index + cnt) % src_buf_size;
+	static uint32_t index;
+	size_t cnt;
+	pwm_audio_write((uint8_t*)(samples + index), AUDIO_SRC_SIZE, &cnt, 0);
+	index = index + cnt;
+	
+	if (index > sizeof(samples))
+	{
+		if (!replay) done = true;
+
+		index = 0;
+	}
 }
 
 void setup()
 {
-	buf = (uint8_t *)malloc(src_buf_size);
-
-	if (buf == NULL)
-	{
-		ESP_LOGE(TAG, "malloc error");
-		vTaskDelay(portMAX_DELAY);
-	}
-
-	for (size_t i = 0; i < src_buf_size; i++)
-	{
-		buf[i] = 128 + (127.8f * sinf(PI_2 * (float)i / (float)src_buf_size));
-		printf("sin%d = %d\n", i, buf[i]);
-	}
+	for (size_t i = 0; i < AUDIO_SRC_SIZE; i++)	buf[i] = 127 * sin(PI_2 * i / AUDIO_SRC_SIZE);
 
 	pwm_audio_config_t pac;
 	pac.duty_resolution = LEDC_TIMER_8_BIT;
@@ -59,13 +49,14 @@ void setup()
 	pac.ledc_timer_sel = LEDC_TIMER_0;
 	pac.tg_num = TIMER_GROUP_0;
 	pac.timer_num = TIMER_0;
-	pac.ringbuf_len = 1024;
+	pac.ringbuf_len = RINGBUFFER_SIZE;
 	pwm_audio_init(&pac);
-	pwm_audio_set_param(48000, LEDC_TIMER_8_BIT, 1);
-	pwm_audio_start();
+	pwm_audio_set_param(sampleRate, LEDC_TIMER_8_BIT, 1);
+    pwm_audio_start();
 }
 
 void loop()
 {
-	sin_test();
+	//sin_test();
+	if (!done) wave_test();
 }
